90fb50af9094d4e32033173edc2c183df628865b
Hamidjon03 <forevalm@gmail.com>
2023-04-02 12:28:56 +0500

    searching method was a little bit complex, featured, company and name filtering added

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,29 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({featured: false})
+
+  const search = 'ab'
+
+  const products = await Product.find({
+    name: {$regex: search, $options: 'i'}
+  })
   res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured} = req.query
+  const {featured, company, name} = req.query
   const queryObj = {}
 
   if(featured){
     queryObj.featured = featured === 'true' ? true : false
+  }
+
+  if (company) {
+    queryObj.company = company
+  }
+
+  if (name) {
+    queryObj.name = {$regex: name, $options: 'i'}
   }
 
   console.log(queryObj)
