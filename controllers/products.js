3fd41124b7c008724b97fe99cc9d87b3ae504f17
Hamidjon03 <forevalm@gmail.com>
2023-04-02 13:06:06 +0500

    sor feature aded

--- controllers/products.js
+++ controllers/products.js
@@ -2,16 +2,14 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
 
-  const search = 'ab'
+  // const search = 'ab'
 
-  const products = await Product.find({
-    name: {$regex: search, $options: 'i'}
-  })
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name} = req.query
+  const {featured, company, name, sort} = req.query
   const queryObj = {}
 
   if(featured){
@@ -26,9 +24,20 @@ const getAllProducts = async (req, res) => {
     queryObj.name = {$regex: name, $options: 'i'}
   }
 
-  console.log(queryObj)
+  // console.log(queryObj)
 
-  const products = await Product.find(queryObj)
+  const result = Product.find(queryObj)
+
+  if (sort) {
+    // console.log(sort)
+    const sortList = sort.split(',').join(' ')
+    // console.log(sortList)
+    result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  const products = await result
   res.status(200).json({products, nbHits: products.length})
 }
 
