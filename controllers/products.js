0bf399b817c3b2d76fa5b4c97cbe07ae830d3453
Hamidjon03 <forevalm@gmail.com>
2023-04-02 13:39:54 +0500

    sort and select was added

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name, sort} = req.query
+  const {featured, company, name, sort, fields} = req.query
   const queryObj = {}
 
   if(featured){
@@ -26,15 +26,18 @@ const getAllProducts = async (req, res) => {
 
   // console.log(queryObj)
 
-  const result = Product.find(queryObj)
-
+  let result = Product.find(queryObj)
+  //sort
   if (sort) {
-    // console.log(sort)
     const sortList = sort.split(',').join(' ')
-    // console.log(sortList)
-    result.sort(sortList)
+    result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
   }
 
   const products = await result
