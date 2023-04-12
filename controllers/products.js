b69fefedb8faee5c95718b5aa9df8aa54d1e9105
Hamidjon03 <forevalm@gmail.com>
2023-04-12 12:20:42 +0500

    completed project

--- controllers/products.js
+++ controllers/products.js
@@ -34,7 +34,15 @@ const getAllProducts = async (req, res) => {
     }
     const regEx = /\b(<|>|>=|=|<|<=)\b/g
     let filters = numericFilters.replace(regEx,(match) => `-${operatorMap[match]}-`)
-    console.log(filters)
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if(options.includes(field)){
+        queryObj[field] = {[operator]: Number(value)}
+      }
+    })
+
+    // console.log(filters)
   }
 
   console.log(queryObj)
